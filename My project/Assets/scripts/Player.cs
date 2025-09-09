4c5bc4c04bc36117291e3064426e4adc623dafb8
Gustavo_Rossi <gustavo.259523@alunosatc.edu.br>
2025-09-09 15:39:27 -0300

    movimento

--- My project/Assets/scripts/Player.cs
+++ My project/Assets/scripts/Player.cs
@@ -1,16 +1,69 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class player : MonoBehaviour
 {
+
+    public Animator anim; 
+
+    private Rigidbody2D rigd;
+    public float speed;
+
+    public float jumpForce = 5f;
+    public bool isground;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rigd = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Jump();
+    }
+
+
+    void Move()
+    {
+        float teclas = Input.GetAxis("Horizontal");
+        rigd.linearVelocity = new Vector2(teclas  * speed, rigd.linearVelocity.y);
+        if (teclas > 0 && isground == true)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            anim.SetInteger("transition", 1);
+        }
+        if (teclas < 0 && isground == true)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            anim.SetInteger("transition", 1);
+        }
+        if (teclas == 0 && isground == true)
+        {
+            anim.SetInteger("transition", 0);
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isground == true)
+        {
+            rigd.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            anim.SetInteger("transition", 2);
+            isground= false;
         
+        }
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "tagGround")
+        {
+            isground = true;   
+        }
     }
 }
