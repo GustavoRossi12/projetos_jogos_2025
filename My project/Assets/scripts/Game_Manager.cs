71d91858a9c8fccbea9a986c01a89046f593176c
Gustavo_Rossi <gustavo.259523@alunosatc.edu.br>
2025-10-14 14:53:16 -0300

    menu

--- My project/Assets/scripts/Game_Manager.cs
+++ My project/Assets/scripts/Game_Manager.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
+using TMPro;
 
 public class Game_Manager : MonoBehaviour
 {
     public int pontos = 0;
     public int vidas = 3;
+    public TextMeshProUGUI textPontos;
 
     public void AddPontos(int qtd)
     {
         pontos += qtd;
 
-        Debug.Log("pontos: " + pontos);
-
         if (pontos < 0)
         {
             pontos = 0;
         }
+
+        textPontos.text = "pontos: " + pontos;
+        Debug.Log("pontos: " + pontos);
     }
 
     public void PerderVidas(int qtd)
